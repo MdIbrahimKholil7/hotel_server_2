5ded2a3ccb0a5e548d2ffb9cab4ceeeb79ed7a21
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-06-30 13:34:14 +0600

    single api create

--- api/roomsApi.js
+++ api/roomsApi.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getAllRoom, postRoom, getSearchRoom, updateRoom } = require('../controllers/roomController');
+const { getAllRoom, postRoom, getSearchRoom, updateRoom, getSingleRoom } = require('../controllers/roomController');
 const router=express.Router()
 
 // get home room 
 router.get('/',getAllRoom)
+// get room by id 
+router.get('/room/:id',getSingleRoom)
 // get search room 
 router.get('/room',getSearchRoom)
 router.put('/room',updateRoom)
--- controllers/roomController.js
+++ controllers/roomController.js
@@ -11,12 +11,25 @@ exports.getAllRoom = async (req, res) => {
         console.log(err)
     }
 }
+
+// get room by id 
+exports.getSingleRoom = async (req, res) => {
+    console.log()
+    try {
+        const result = await Room.findById({_id:req.params.id})
+        res.send(result)
+        console.log(result)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
 // get search room 
 exports.getSearchRoom = async (req, res) => {
     const room = req.query.room
     console.log(room)
     try {
-        const result = await Room.find({city: room })
+        const result = await Room.find({ city: room })
         console.log(result)
         res.send(result)
     } catch (err) {
@@ -24,9 +37,9 @@ exports.getSearchRoom = async (req, res) => {
     }
 }
 // update room 
-exports.updateRoom=async(req,res)=>{
+exports.updateRoom = async (req, res) => {
     try {
-        const result = await Room.updateMany({ roomType: /room/i },{$set:{ratings:5}},{upsert:true,setDefaultsOnInsert:true})
+        const result = await Room.updateMany({ roomType: /room/i }, { $set: { ratings: 5 } }, { upsert: true, setDefaultsOnInsert: true })
         console.log(result)
         res.send(result)
     } catch (err) {
